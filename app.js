bc574ce4f2265e74aa78a8b53cdf2265e3898267
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-14 12:01:42 +0430

    count

--- app.js
+++ app.js
@@ -5,5 +5,19 @@ let count = 0;
 
 // select value and buttons
 
-const value = document.querySelector('#value')
-const btn = document.querySelectorAll('.btn')
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function (event) {
+        const styles = event.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        } else if(styles.contains("increase")){
+            count ++;
+        } else{
+            count = 0;
+        }
+        value.textContent = count;
+    });
+});
