7a752febdd385b7bacb500201f3621299456feac
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-14 12:09:52 +0430

    count

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
 ### Counter
+
+
+
+![alt text](<Screenshot 2024-02-14 120841.png>) 
+
+
+![alt text](<Screenshot 2024-02-14 120812.png>)
+
+
+ ![alt text](<Screenshot 2024-02-14 120823.png>)
--- app.js
+++ app.js
@@ -17,7 +17,16 @@ btns.forEach(function(btn){
             count ++;
         } else{
             count = 0;
+        };
+
+        if(count > 0){
+            value.style.color = "green"
+        } if (count < 0){
+            value.style.color = "blue"
+        } if (count === 0){
+            value.style.color = "black"
         }
+
         value.textContent = count;
     });
 });
